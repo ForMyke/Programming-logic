8b29ae65a2efe8f45dc7568928bed26bb1def594
ForMyke <golomian16@hotmail.com>
2024-07-10 22:35:48 -0600

    Relative Ranks

---
+++ Relative.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function (score) {
+  let sortedScores = [...score].sort((a, b) => b - a);
+  let rankMap = {};
+
+  for (let i = 0; i < sortedScores.length; i++) {
+    if (i === 0) {
+      rankMap[sortedScores[i]] = "Gold Medal";
+    } else if (i === 1) {
+      rankMap[sortedScores[i]] = "Silver Medal";
+    } else if (i === 2) {
+      rankMap[sortedScores[i]] = "Bronze Medal";
+    } else {
+      rankMap[sortedScores[i]] = (i + 1).toString();
+    }
+  }
+
+  return score.map((elemento) => rankMap[elemento]);
+};
+
+//Entrada
+let score = [5, 4, 3, 2, 1];
+let final = findRelativeRanks(score);
+console.log(final);
