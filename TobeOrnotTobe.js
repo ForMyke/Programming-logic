54554630bb497dab07215805f10e118d53171d9d
ForMyke <golomian16@hotmail.com>
2024-07-12 00:04:09 -0600

    TobeOrnot

--- TobeOrnotTobe.js
+++ TobeOrnotTobe.js
@@ -20,27 +20,3 @@ var expect = function (val) {
     },
   };
 };
-
-// Ejemplo de uso:
-
-try {
-  let entrada = expect(5).toBe(5); // devuelve 5
-  let salida = expect(entrada); // permite continuar con el valor de entrada
-
-  console.log(salida.toBe(5)); // true
-  console.log(salida.notToBe(10)); // true
-} catch (error) {
-  console.error(error.message);
-}
-
-try {
-  let entrada = expect(5).toBe(10); // lanza un error "Not Equal"
-} catch (error) {
-  console.error(error.message);
-}
-
-try {
-  let entrada = expect(5).notToBe(5); // lanza un error "Equal"
-} catch (error) {
-  console.error(error.message);
-}
